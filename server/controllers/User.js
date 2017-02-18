fb4b0655362cb8dce50961c8c0da03b5f58b6c41
yerevin <sirmmate@gmail.com>
2017-02-18 17:24:50 +0100

    change password implementation

--- server/controllers/User.js
+++ server/controllers/User.js
@@ -3,6 +3,8 @@
 const Joi = require('joi');
 const UserModel = require('../models/User');
 const mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+var SALT_WORK_FACTOR = 10;
 
 exports.getAll = {
 	cors: {
@@ -233,25 +235,52 @@ exports.changePassword = {
 			id: Joi.string().required()
 		},
 		payload: {
-			password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required()
+			oldpassword: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required(),
+			password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required(),
+			password_confirmation: Joi.any().valid(Joi.ref('password')).required()
 		}
 	},
-	handler: function (request, reply) {
-		UserModel.findOneAndUpdate({_id: request.params.id}, request.payload, function (error, data) {
-			if (error) {
-				reply({
-					statusCode: 503,
-					message: 'Failed to get data',
-					data: error
-				});
-			} else {
-				reply({
-					statusCode: 200,
-					message: 'User Updated Successfully',
-					data: data
-				});
-			}
-		});
+    handler: function (request, reply) {
+		UserModel.findOne({_id: request.params.id}, function(err, User) {
+		    if (err) throw err;
+		    User.comparePassword(request.payload.oldpassword, function(err, isMatch) {
+		        if (err) {
+	                reply({
+	                    statusCode: 503,
+	                    message: 'Failed to get data',
+	                    data: err
+	                });
+	            }
+				else if (isMatch) {
+					    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+					        if (err) return next(err);
+					        bcrypt.hash(request.payload.password, salt, function(err, hash) {
+					            if (err) return next(err);
+								UserModel.findOneAndUpdate({_id: request.params.id}, {password: hash}, function (error, data) {
+									if (error) {
+										reply({
+											statusCode: 503,
+											message: 'Failed to get data',
+											data: error
+										});
+									} else {
+										reply({
+											statusCode: 200,
+											message: 'Password changed Successfully'
+										});
+									}
+								});
+					        });
+					    });
 
-	}
+	            }
+	            else {
+	                reply({
+	                    statusCode: 403,
+	                    message: 'User Bad Password'
+	                });
+	            }
+		    });
+		});
+   }
 };
--- server/models/User.js
+++ server/models/User.js
@@ -16,8 +16,7 @@ var UserSchema = new mongoose.Schema({
     required: true
   },
   name: {
-    type: String,
-    required: true
+    type: String
   },
   avatar: {
   	type: String
@@ -34,25 +33,24 @@ var UserSchema = new mongoose.Schema({
 UserSchema.pre('save', function(next) {
     var User = this;
 
-// only hash the password if it has been modified (or is new)
-if (!User.isModified('password')) return next();
-
-// generate a salt
-bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-    if (err) return next(err);
-
-    // hash the password using our new salt
-    bcrypt.hash(User.password, salt, function(err, hash) {
+  // only hash the password if it has been modified (or is new)
+ 
+  if (!User.isModified('password')) return next();
+        // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
         if (err) return next(err);
 
-        // override the cleartext password with the hashed one
-        User.password = hash;
-        next();
+        // hash the password using our new salt
+        bcrypt.hash(User.password, salt, function(err, hash) {
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            User.password = hash;
+            next();
+        });
     });
-});
+}); 
 
-
-});
 
 UserSchema.methods.comparePassword = function(candidatePassword, cb) {
     bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
